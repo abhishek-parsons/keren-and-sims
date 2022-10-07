1f1300567b2de20cdbb6c84bcb4c048db6f3866c
abhishek-parsons <kalia645@newschool.edu>
2022-10-07 17:02:10 -0400

    Add loader animation + Keren's face

---
+++ script.js
@@ -0,0 +1,6 @@
+// hide loader after a few seconds
+var loader = document.getElementById("loader");
+setTimeout(function () {
+  loader.classList.add("animate__animated");
+  loader.classList.add("animate__fadeOut");
+}, 4000);
