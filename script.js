7b3aba4042c068f2ba458802fadf459c488c95a0
abhishek-parsons <kalia645@newschool.edu>
2022-10-09 19:38:16 -0400

    Add question on click

--- script.js
+++ script.js
@@ -3,4 +3,45 @@ var loader = document.getElementById("loader");
 setTimeout(function () {
   loader.classList.add("animate__animated");
   loader.classList.add("animate__fadeOut");
+  setTimeout(function () {
+    loader.remove();
+  }, 1000);
 }, 1000);
+
+function clickFace() {
+  //show q1
+  var q1 = document.getElementsByClassName("div2")[0];
+  q1.style.display = "block";
+  q1.classList.add("animate__animated");
+  q1.classList.add("animate__bounceIn");
+
+  //show q2
+  var q2 = document.getElementsByClassName("div3")[0];
+  q2.style.display = "block";
+  q2.classList.add("animate__animated");
+  q2.classList.add("animate__bounceIn");
+
+  //show q1
+  var q3 = document.getElementsByClassName("div4")[0];
+  q3.style.display = "block";
+  q3.classList.add("animate__animated");
+  q3.classList.add("animate__bounceIn");
+
+  //show q1
+  var q4 = document.getElementsByClassName("div5")[0];
+  q4.style.display = "block";
+  q4.classList.add("animate__animated");
+  q4.classList.add("animate__bounceIn");
+
+  //show q1
+  var q5 = document.getElementsByClassName("div6")[0];
+  q5.style.display = "block";
+  q5.classList.add("animate__animated");
+  q5.classList.add("animate__bounceIn");
+
+  //show q1
+  var q6 = document.getElementsByClassName("div7")[0];
+  q6.style.display = "block";
+  q6.classList.add("animate__animated");
+  q6.classList.add("animate__bounceIn");
+}
